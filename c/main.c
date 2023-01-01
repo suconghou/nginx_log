ae7bf2ba018e158db79c1861d3e1a6fca38cf0db
sucongohu <1126045770@qq.com>
2023-01-01 16:05:45 +0800

    init

---
+++ c/main.c
@@ -0,0 +1,381 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef int (*char_is_match)(unsigned char x, unsigned char y, unsigned char z);
+
+// 此变量解析每行时需置为0，模拟闭包
+int space_count = 0;
+// 当前字符是双引号，下个字符是空格，上个字符是http版本,并且只能包含2个空格
+int request_line_quote_end(unsigned char x, unsigned char y, unsigned char z)
+{
+    if (x == 32)
+    {
+        space_count++;
+    }
+    if (space_count > 2)
+    {
+        return 0;
+    }
+    if (x == 34 && y == 32 && (z == 50 || z == 49 || z == 48))
+    {
+        return 0;
+    }
+    return 1;
+}
+
+// 仅数字
+int digital(unsigned char x, unsigned char y, unsigned char z)
+{
+    return x >= 48 && x <= 57;
+}
+
+// 包含数字和.号
+int digital_dot(unsigned char x, unsigned char y, unsigned char z)
+{
+    return (x >= 48 && x <= 57) || x == 46;
+}
+
+// 包含数字和.号或-号
+int digital_dot_minus(unsigned char x, unsigned char y, unsigned char z)
+{
+    return (x >= 48 && x <= 57) || x == 46 || x == 45;
+}
+
+// 匹配到],并且下一个是空格
+int square_right_space(unsigned char x, unsigned char y, unsigned char z)
+{
+    if (x == 93 && y == 32)
+    {
+        return 0;
+    }
+    return 1;
+}
+
+// 非空格
+int not_space(unsigned char x, unsigned char y, unsigned char z)
+{
+    return x != 32;
+}
+
+// 双引号后面是空格
+int quote_string_end(unsigned char x, unsigned char y, unsigned char z)
+{
+    if (x == 34 && y == 32)
+    {
+        return 0;
+    }
+    return 1;
+}
+
+// 当前字符是空格，上个字符是字母,不包含空格
+int string_end(unsigned char x, unsigned char y, unsigned char z)
+{
+    if (x == 32 && ((z >= 65 && z <= 90) || (z >= 97 && z <= 122)))
+    {
+        return 0;
+    }
+    return 1;
+}
+
+// 当前是空格，上一个是-或者数字
+int digital_or_none_end(unsigned char x, unsigned char y, unsigned char z)
+{
+    if (x == 32 && ((z >= 48 && z <= 57) || z == 45))
+    {
+        return 0;
+    }
+    return 1;
+}
+
+// offset 字符串坐标
+int parse_item_trim_space(const char *s, int *offset, int len, char *item_value, char_is_match cond, int strip_square)
+{
+    int pad = 1;
+    int found_start = -1;
+    int found_end = -1;
+    int gotvalue = -1;
+    int i = *offset;
+    while (i < len)
+    {
+        unsigned char x = s[i];
+        i++;
+        if ((x == ' ' || (strip_square && x == '[')) && pad)
+        {
+            *offset = i;
+            continue;
+        }
+        else
+        {
+            pad = 0;
+        }
+        if (gotvalue < 0)
+        {
+            unsigned char y = i < len ? s[i] : 0;
+            unsigned char z = i >= 2 ? s[i - 2] : 0;
+            if (cond(x, y, z))
+            {
+                found_end = i - 1;
+                if (found_start < 0)
+                {
+                    found_start = found_end;
+                }
+                if (i < len)
+                {
+                    continue;
+                }
+            }
+            if (found_start < 0)
+            {
+                goto end;
+            }
+            int v_len = found_end - found_start + 1;
+            strncpy(item_value, s + found_start, v_len);
+            item_value[v_len] = '\0';
+            gotvalue = 1;
+            if (i >= len)
+            {
+                if (found_end == len - 1 || x == ' ')
+                {
+                    // 字符串已完全遍历
+                    *offset = len;
+                }
+                else
+                {
+                    *offset = found_end + 1;
+                }
+            }
+        }
+        else
+        {
+            if (x == ' ' || (strip_square && x == ']'))
+            {
+                if (i < len)
+                {
+                    continue;
+                }
+                else if (i == len)
+                {
+                    i++;
+                }
+            }
+            *offset = i - 1;
+            break;
+        }
+    }
+end:
+    return found_start;
+}
+
+int parse_remote_addr(const char *s, int *offset, int len, char *item_value)
+{
+    return parse_item_trim_space(s, offset, len, item_value, digital_dot, 0);
+}
+
+int parse_remote_user(const char *s, int *offset, int len, char *item_value)
+{
+    int i = *offset;
+    while (i < len)
+    {
+        if (s[i] == '-')
+        {
+            i++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    *offset = i;
+    return parse_item_trim_space(s, offset, len, item_value, not_space, 0);
+}
+
+int parse_time_local(const char *s, int *offset, int len, char *item_value)
+{
+    return parse_item_trim_space(s, offset, len, item_value, square_right_space, 1);
+}
+
+int parse_request_line(const char *s, int *offset, int len, char *item_value)
+{
+    space_count = 0;
+    return parse_item_trim_space(s, offset, len, item_value, request_line_quote_end, 0);
+}
+
+int parse_status_code(const char *s, int *offset, int len, char *item_value)
+{
+    return parse_item_trim_space(s, offset, len, item_value, digital, 0);
+}
+
+int parse_body_bytes_sent(const char *s, int *offset, int len, char *item_value)
+{
+    return parse_item_trim_space(s, offset, len, item_value, digital, 0);
+}
+
+int parse_http_referer(const char *s, int *offset, int len, char *item_value)
+{
+    return parse_item_trim_space(s, offset, len, item_value, quote_string_end, 0);
+}
+
+int parse_http_user_agent(const char *s, int *offset, int len, char *item_value)
+{
+    return parse_item_trim_space(s, offset, len, item_value, quote_string_end, 0);
+}
+
+int parse_http_x_forwarded_for(const char *s, int *offset, int len, char *item_value)
+{
+    return parse_item_trim_space(s, offset, len, item_value, quote_string_end, 0);
+}
+
+int parse_host(const char *s, int *offset, int len, char *item_value)
+{
+    return parse_item_trim_space(s, offset, len, item_value, string_end, 0);
+}
+
+int parse_request_length(const char *s, int *offset, int len, char *item_value)
+{
+    return parse_item_trim_space(s, offset, len, item_value, digital, 0);
+}
+
+int parse_bytes_sent(const char *s, int *offset, int len, char *item_value)
+{
+    return parse_item_trim_space(s, offset, len, item_value, digital, 0);
+}
+
+int parse_upstream_addr(const char *s, int *offset, int len, char *item_value)
+{
+    return parse_item_trim_space(s, offset, len, item_value, digital_or_none_end, 0);
+}
+
+int parse_upstream_status(const char *s, int *offset, int len, char *item_value)
+{
+    return parse_item_trim_space(s, offset, len, item_value, digital_or_none_end, 0);
+}
+
+int parse_request_time(const char *s, int *offset, int len, char *item_value)
+{
+    return parse_item_trim_space(s, offset, len, item_value, digital_dot, 0);
+}
+
+int parse_upstream_response_time(const char *s, int *offset, int len, char *item_value)
+{
+    return parse_item_trim_space(s, offset, len, item_value, digital_dot_minus, 0);
+}
+
+int parse_upstream_connect_time(const char *s, int *offset, int len, char *item_value)
+{
+    return parse_item_trim_space(s, offset, len, item_value, digital_dot_minus, 0);
+}
+
+int parse_upstream_header_time(const char *s, int *offset, int len, char *item_value)
+{
+    return parse_item_trim_space(s, offset, len, item_value, digital_dot_minus, 0);
+}
+
+int process_line(char *line)
+{
+}
+
+int main()
+{
+    FILE *fp = fopen("/tmp/1", "r");
+    if (fp == NULL)
+    {
+        return -1;
+    }
+    char s[8192];
+    int linecount = 0;
+    while (fgets(s, sizeof s, fp) != NULL)
+    {
+        int offset = 0;
+        char value[8192] = {0};
+        int len = strlen(s);
+        int a = parse_remote_addr(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("remote_addr:%s\n", value);
+
+        a = parse_remote_user(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("remote_user:%s\n", value);
+
+        a = parse_time_local(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("time_local:%s\n", value);
+
+        a = parse_request_line(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("request_line:%s\n", value);
+
+        a = parse_status_code(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("status_code:%s\n", value);
+
+        a = parse_body_bytes_sent(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("body_bytes_sent:%s\n", value);
+
+        a = parse_http_referer(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("http_referer:%s\n", value);
+
+        a = parse_http_user_agent(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("http_user_agent:%s\n", value);
+
+        a = parse_http_x_forwarded_for(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("http_x_forwarded_for:%s\n", value);
+
+        a = parse_host(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("host:%s\n", value);
+
+        a = parse_request_length(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("request_length:%s\n", value);
+
+        a = parse_bytes_sent(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("bytes_sent:%s\n", value);
+
+        a = parse_upstream_addr(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("upstream_addr:%s\n", value);
+
+        a = parse_upstream_status(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("upstream_status:%s\n", value);
+
+        a = parse_request_time(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("request_time:%s\n", value);
+
+        a = parse_upstream_response_time(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("upstream_response_time:%s\n", value);
+
+        a = parse_upstream_connect_time(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("upstream_connect_time:%s\n", value);
+
+        a = parse_upstream_header_time(s, &offset, len, value);
+        // printf("offset:%d\n", offset);
+        // printf("index:%d\n", a);
+        printf("upstream_header_time:%s\n", value);
+    }
+}
---
+++ main.nim
@@ -0,0 +1,345 @@
+
+
+import tables,sets,strutils
+
+
+type Line = object
+    index:int
+    str:string
+
+const blank = {' '}
+const minus = {'-'}
+const quotation = {'"'}
+const square_left = {' ','['}
+const square_right = {' ',']'}
+const digital_and_dot = {'0'..'9','.'}
+const digital_and_minus = {'0'..'9','-'}
+const digital_and_dot_and_minus = {'0'..'9','.','-'}
+
+# TODO , 可以优化掉substr, 内部记录index
+# 根据条件解析，并去除前置后置x字符
+proc parse_item_trimx(this:var Line,left:set[char],right:set[char],cond:proc):string=
+    var i =0;
+    var start=0;
+    var item_value:string;
+    var pad = true
+    var found = -1;
+    while i < this.str.len:
+        let x = this.str[i]
+        i+=1; # i已指向下一个字符
+        if x in left and pad:
+            # 去前置连续x
+            start+=1;
+            continue
+        else:
+            pad = false
+        if item_value.len < 1:
+            let y = if i<this.str.len: this.str[i] else: '\x00'
+            let z = if i>=2: this.str[i-2] else: '\x00'
+            # 符合预定格式,x为当前字符,y为下个字符,y可能为0,z为上一个字符
+            if cond(x,y,z):
+                found = i-1
+                # 如果未到行末尾,才能continue,否则i=len了,不能continue,会造成下次退出循环,item_value未赋值
+                if i<this.str.len:
+                    continue
+            # 否则匹配到边界或者完全没有匹配到
+            if found < 0:
+                # 完全没有匹配到
+                raise newException(ValueError,"匹配失败:"&this.str)
+            # 包含当前字符c
+            item_value = this.str.substr(start,found)
+            # 执行到此处，已匹配到了想要的字符串，要么匹配到字符串结尾了，要么中途中断，要么匹配到最后一个字符了但不符合
+            if i>=this.str.len:
+                # 如果中途中断，则不会进入此条件，下次循环会进去后置特定字符逻辑
+                # 如果最后一个字符符合，则found=len-1,此时剩余应为空
+                # 如果最后一个字符不符合，判断是否是后置字符，如果是则也为空
+                if found==this.str.len-1 or x in right:
+                    this.str = ""
+                else:
+                    this.str = this.str.substr(found+1)
+        else:
+            if x in right:
+                # 去后置特定字符,如果当前字符就是最后一个字符，并且当前字符是特定字符，则i需等于len,以完全裁剪
+                if i < this.str.len :
+                    continue;
+                elif i == this.str.len :
+                    i+=1
+            this.str = this.str.substr(i-1)
+            break
+    # 防止前置字符去除时，直接continue完所有
+    if item_value.len < 1:
+        raise newException(ValueError,"匹配失败:"&this.str)
+    return item_value
+
+# 仅数字
+proc digital(x:char,y:char,z:char):bool = x in Digits
+
+# 包含数字和.号
+proc digital_dot(x:char,y:char,z:char):bool = x in digital_and_dot
+
+# 包含数字和.号或-号
+proc digital_dot_minus(x:char,y:char,z:char):bool = x in digital_and_dot_and_minus
+
+# 匹配到],并且下一个是空格
+proc square_right_space(x:char,y:char,z:char):bool = not (x=='\93' and y=='\32')
+
+# 非空格
+proc not_space(x:char,y:char,z:char):bool = x!='\32' 
+
+# 双引号后面是空格
+proc quote_string_end(x:char,y:char,z:char):bool = not (x=='\34' and y=='\32')
+
+# 当前字符是空格，上个字符是字母,不包含空格
+proc string_end(x:char,y:char,z:char):bool = not (x=='\32' and ( z in Letters ))
+
+# 当前是空格，上一个是-或者数字
+proc digital_or_none_end(x:char,y:char,z:char):bool = not ( x == '\32' and ( z in digital_and_minus ) )
+
+# 包含数字和.号
+proc parse_remote_addr(this:var Line):string=
+    return this.parse_item_trimx(blank,blank,digital_dot)
+
+# 去除可能存在的-,非空格
+proc parse_remote_user(this:var Line):string =
+    this.str = this.str.strip(true,false,minus)
+    return this.parse_item_trimx(blank,blank,not_space)
+
+# 匹配到],并且下一个是空格
+proc parse_time_local(this:var Line):string =
+    return this.parse_item_trimx(square_left,square_right,square_right_space)
+
+# 当前字符是双引号，下个字符是空格，上个字符是http版本,并且只能包含2个空格
+proc parse_request_line(this:var Line):string=
+    var c = 0;
+    proc v(x:char,y:char,z:char):bool =
+        if x == '\32':
+            c+=1
+        if c > 2 :
+            return false
+        return not (x=='\34' and y=='\32' and (z=='\50' or z=='\49' or z=='\48') )
+    let a = this.parse_item_trimx(blank,blank,v)
+    return a.strip(true,true,quotation)
+
+# 是数字
+proc parse_status_code(this:var Line):string = 
+    return this.parse_item_trimx(blank,blank,digital)
+
+# 是数字
+proc parse_body_bytes_sent(this:var Line):string =
+    return this.parse_item_trimx(blank,blank,digital)
+
+# 当前字符是双引号，下个字符是空格
+proc parse_http_referer(this:var Line):string =
+    let x= this.parse_item_trimx(blank,blank,quote_string_end)
+    return x.strip(true,true,quotation)
+
+# 当前字符是双引号，下个字符是空格
+proc parse_http_user_agent(this:var Line):string =
+    let x= this.parse_item_trimx(blank,blank,quote_string_end)
+    return x.strip(true,true,quotation)
+
+# 当前字符是双引号，下个字符是空格
+proc parse_http_x_forwarded_for(this:var Line):string =
+    let x = this.parse_item_trimx(blank,blank,quote_string_end)
+    return x.strip(true,true,quotation)
+
+# 当前字符是空格，上个字符是字母
+proc parse_host(this:var Line):string =
+    return this.parse_item_trimx(blank,blank,string_end)
+
+# 数字
+proc parse_request_length(this:var Line):string =
+    return this.parse_item_trimx(blank,blank,digital)
+
+# 数字
+proc parse_bytes_sent(this:var Line):string =
+    return this.parse_item_trimx(blank,blank,digital)
+
+# 当前是空格，上一个是-或者数字
+proc parse_upstream_addr(this:var Line):string =
+    return this.parse_item_trimx(blank,blank,digital_or_none_end)
+
+# 当前是空格，上一个是-或者数字
+proc parse_upstream_status(this:var Line):string =
+    return this.parse_item_trimx(blank,blank,digital_or_none_end)
+
+# 数字和.号
+proc parse_request_time(this:var Line):string =
+    return this.parse_item_trimx(blank,blank,digital_dot)
+
+# 数字和.号，或者-
+proc parse_upstream_response_time(this:var Line):string =
+    return this.parse_item_trimx(blank,blank,digital_dot_minus)
+
+# 数字和.号，或者-
+proc parse_upstream_connect_time(this:var Line):string =
+    return this.parse_item_trimx(blank,blank,digital_dot_minus)
+
+# 数字和.号，或者-
+proc parse_upstream_header_time(this:var Line):string =
+    return this.parse_item_trimx(blank,blank,digital_dot_minus)
+
+
+
+proc process_line(line:string)=
+    echo line;
+
+
+proc process(filename:string)=
+    var remote_addr_data:OrderedTable[string, int];
+    var remote_user_data:OrderedTable[string,int];
+    var status_data:OrderedTable[string,int];
+    var time_local_data:Table[string,Table[string,int]];
+    var statusMap:Table[string,Table[string,int]];
+    var http_referer_data:OrderedTable[string,int];
+    var http_user_agent_data:OrderedTable[string,int];
+    var http_x_forwarded_for_data:OrderedTable[string,int];
+    var http_method_data:OrderedTable[string,int];
+    var request_uri_data:OrderedTable[string,int];
+    var http_version_data:OrderedTable[string,int];
+    var url_bytes_data:OrderedTable[string,int];
+    var total_bytes_recv :uint = 0;
+    var total_bytes_sent :uint = 0;
+    var total_lines:uint = 0;
+
+
+    proc parse_line(line:string)=
+        var l = Line(str :line)
+        let remote_addr = l.parse_remote_addr()
+        let remote_user = l.parse_remote_user()
+        let time_local = l.parse_time_local()
+        let request_line = l.parse_request_line()
+        let status_code = l.parse_status_code()
+        let body_bytes_sent = l.parse_body_bytes_sent()
+        let http_referer = l.parse_http_referer()
+        let http_user_agent = l.parse_http_user_agent()
+        let http_x_forwarded_for = l.parse_http_x_forwarded_for()
+        let host = l.parse_host()
+        let request_length = l.parse_request_length()
+        let bytes_sent = l.parse_bytes_sent()
+        let upstream_addr = l.parse_upstream_addr()
+        let upstream_status = l.parse_upstream_status()
+        let request_time = l.parse_request_time()
+        let upstream_response_time = l.parse_upstream_response_time()
+        let upstream_connect_time = l.parse_upstream_connect_time()
+        let upstream_header_time = l.parse_upstream_header_time()
+
+        let bytes_sent_num = parseUint(bytes_sent)
+        let request_length_num = parseUInt(request_length)
+
+        total_bytes_sent+=bytes_sent_num
+        total_bytes_recv+=request_length_num
+
+
+        var n= remote_addr_data.getOrDefault(remote_addr,0)
+        remote_addr_data[remote_addr]=n+1
+        n = remote_user_data.getOrDefault(remote_user,0)
+        remote_user_data[remote_user]=n+1
+        n = status_data.getOrDefault(status_code,0)
+        status_data[status_code]=n+1
+        n = http_referer_data.getOrDefault(http_referer,0)
+        http_referer_data[http_referer]=n+1
+        n = http_user_agent_data.getOrDefault(http_user_agent,0)
+        http_user_agent_data[http_user_agent]=n+1
+        n=http_x_forwarded_for_data.getOrDefault(http_x_forwarded_for,0)
+        http_x_forwarded_for_data[http_x_forwarded_for]=n+1
+
+
+    for line in filename.lines:
+        try:
+            parse_line(line);
+            total_lines+=1;
+        except:
+            echo line
+            
+
+
+proc test1(line:string)= 
+    var l = Line(str :line)
+    let remote_addr = l.parse_remote_addr()
+    # echo "remote_addr:["&remote_addr&"]"
+    # echo l.str
+    let remote_user = l.parse_remote_user()
+    # echo "remote_user:["&remote_user&"]"
+    # echo l.str
+    let time_local = l.parse_time_local()
+    # echo "time_local:["&time_local&"]"
+    # echo l.str
+    let request_line = l.parse_request_line()
+    # echo request_line
+    # echo "request_line:["&request_line&"]"
+    # echo l.str
+    let status_code = l.parse_status_code()
+    # echo "status_code:["&status_code&"]"
+    # echo l.str
+
+    let body_bytes_sent = l.parse_body_bytes_sent()
+    # echo body_bytes_sent
+    # echo "body_bytes_sent:["&body_bytes_sent&"]"
+    # echo l.str
+
+    let http_referer = l.parse_http_referer()
+    # echo "http_referer:["&http_referer&"]"
+    # echo l.str
+
+    let http_user_agent = l.parse_http_user_agent()
+    # echo http_user_agent
+    # echo "http_user_agent:["&http_user_agent&"]"
+    # echo l.str
+
+    let http_x_forwarded_for = l.parse_http_x_forwarded_for()
+    # echo "http_x_forwarded_for:["&http_x_forwarded_for&"]"
+    # echo l.str
+
+    
+    let host = l.parse_host()
+    # echo "host:["&host&"]"
+    # echo l.str
+
+    let request_length = l.parse_request_length()
+    # echo request_length
+    # echo "request_length:["&request_length&"]"
+    # echo l.str
+
+    let bytes_sent = l.parse_bytes_sent()
+    # echo "bytes_sent:["&bytes_sent&"]"
+    # echo l.str
+
+    let upstream_addr = l.parse_upstream_addr()
+    # echo "upstream_addr:["&upstream_addr&"]"
+    # echo l.str
+
+    let upstream_status = l.parse_upstream_status()
+    # echo "upstream_status:["&upstream_status&"]"
+    # echo l.str
+
+    let request_time = l.parse_request_time()
+    # echo "request_time:["&request_time&"]"
+    # echo l.str
+
+    
+    let upstream_response_time = l.parse_upstream_response_time()
+
+    # echo "["&upstream_response_time&"]"
+    # echo l.str
+
+
+    
+    let upstream_connect_time = l.parse_upstream_connect_time()
+    # echo "[upstream_connect_time:"&upstream_connect_time&"]"
+    # echo l.str
+
+    
+    let upstream_header_time = l.parse_upstream_header_time()
+    # echo "[upstream_header_time:"&upstream_header_time&"]"
+    # echo "["&l.str&"]"
+
+    
+let name = "/tmp/22"
+for line in name.lines:
+    try:
+        test1(line)
+    except:
+        stderr.writeLine(getCurrentExceptionMsg())
+        stderr.writeLine(line)
+
